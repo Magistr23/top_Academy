2eafbc6b88ba24246f0fa8663c274ea912b97421
Иван Сорокин <spanhbob98@mail.ru>
2023-12-26 18:59:34 +0300

    доработка с фильтрами

--- hous/app/js/mobile.js
+++ hous/app/js/mobile.js
@@ -1,45 +1,49 @@
 function mobMenu() {
 (function ($) {
     $(document).ready(function () {
-        $(".mob-menu, .js-blackout").toggleClass("active");
+        $(".mob-menu, .js-blackout").toggleClass("active-menu");
     });
   })(jQuery)
+}
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("reproductions__item");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
 }
 
-function filterFranc() {
-  (function ($) {
-    $(document).ready(function () {
-        $(".franc, .js-blackout").toggleClass("display-block");
-        $(".germ, .js-blackout").toggleClass("display-none");
-        $(".angl, .js-blackout").toggleClass("display-none");
-    });
-  })(jQuery)
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
 }
 
-function filterGerm() {
-  (function ($) {
-    $(document).ready(function () {
-        $(".franc, .js-blackout").toggleClass("display-none");
-        $(".germ, .js-blackout").toggleClass("display-block");
-        $(".angl, .js-blackout").toggleClass("display-none");
-    });
-  })(jQuery)
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
 }
-function filterAngl() {
-  (function ($) {
-    $(document).ready(function () {
-        $(".franc, .js-blackout").toggleClass("display-none");
-        $(".germ, .js-blackout").toggleClass("display-none");
-        $(".angl, .js-blackout").toggleClass("display-block");
-    });
-  })(jQuery)
-}
-function filterFull() {
-  (function ($) {
-    $(document).ready(function () {
-        $(".franc, .js-blackout").toggleClass("display-block");
-        $(".germ, .js-blackout").toggleClass("display-block");
-        $(".angl, .js-blackout").toggleClass("display-block");
-    });
-  })(jQuery)
+
+// Добавьте активный класс к текущей кнопке (выделите его)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("reproductions__btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
